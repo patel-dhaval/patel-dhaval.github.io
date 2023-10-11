520179c3deba146056d14612648c37c7adcb58ef
Dhaval Patel <dhavaljp.95@gmail.com>
2023-10-11 01:49:49 -0400

    Project Section Live!

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,6 +213,25 @@
     }
   });
 
+/*----------------------------------------------------*/
+/*	Modal Popup
+------------------------------------------------------*/
+
+  $('.portfolio-wrap a').magnificPopup({
+
+    type:'inline',
+    fixedContentPos: false,
+    removalDelay: 200,
+    showCloseBtn: false,
+    mainClass: 'mfp-fade'
+
+  });
+
+  $(document).on('click', '.popup-modal-dismiss', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
   /**
    * Testimonials slider
    */
